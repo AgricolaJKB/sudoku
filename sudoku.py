ca02d54faf6765d4e23852c4feb7b4111dd3d429
AgricolaJKB <64554282+AgricolaJKB@users.noreply.github.com>
2021-01-06 20:47:52 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import os, time
 
 def openDriver(url):
-    chromeOptions = webdriver.ChromeOptions()
-    #chromeOptions.add_argument("--headless")
+    chromeOptions = Options()
+    chromeOptions.add_experimental_option("detach", True)
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chromeOptions)
     driver.set_window_position(0, 0)
-    driver.set_window_size(1200, 700)
+    driver.set_window_size(1300, 800)
 
     driver.get(url)
 
@@ -36,6 +38,9 @@ def scrapeInitial(driver, email, password):
     driver.find_element_by_id('login_email').send_keys(email)
     driver.find_element_by_id('login_pass').send_keys(password)
     waitAndClick(driver, '//*[@id="login"]/div[4]/input')
+    
+    # Spiel starten
+    waitAndClick(driver, '//*[@id="content"]/div[2]/app-game-grid/div/button')
 
     # Startkonstellation scrapen
     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'fixed-value')))
@@ -43,7 +48,7 @@ def scrapeInitial(driver, email, password):
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-    result = [[[] for _ in range(9)] for _ in range(9)]
+    result = [[0 for _ in range(9)] for _ in range(9)]
 
     for rowCounter, row in enumerate(soup.find('div', class_='sodokoGrid').children):
         try:
@@ -51,11 +56,21 @@ def scrapeInitial(driver, email, password):
                 try:
                     result[rowCounter][columnCounter] = int(column.get_text())
                 except ValueError:
-                    result[rowCounter][columnCounter] = 0
+                    pass
         except AttributeError:
             pass
 
-    return result
+    return result, soup
+
+def insertResult(driver, result, soup):
+    for rowCounter, row in enumerate(result):
+        for columnCounter, column in enumerate(row):
+            try:
+                element = driver.find_element_by_id(f'{rowCounter}{columnCounter}')
+                element.click()
+                element.send_keys(str(column))
+            except NoSuchElementException:
+                pass
 
 def main():
     url = 'https://sudoku.zeit.de'
@@ -65,10 +80,26 @@ def main():
         password = file.read()
 
     driver = openDriver(url)
-    result = scrapeInitial(driver, email, password)
-    for row in result:
-        print(row)
-    driver.close()
+    startingGrid, soup = scrapeInitial(driver, email, password)
+
+    print(startingGrid)
+    # Hier fehlt dein Algorithmus. startingGrid enthält die Startaufstellung (als Liste von Listen), das Ergebnis
+    # muss dann in result abgespeichert werden.
+    #
+    # Format für In- und Output:
+    # [[0, 0, 0, 0, 5, 4, 0, 0, 8]
+    # [0, 6, 0, 0, 0, 0, 0, 1, 7]
+    # [0, 0, 0, 9, 0, 0, 0, 0, 0]
+    # [6, 0, 5, 0, 0, 7, 0, 0, 0]
+    # [0, 0, 0, 0, 0, 0, 1, 0, 0]
+    # [0, 0, 0, 0, 9, 0, 0, 7, 3]
+    # [4, 0, 0, 0, 0, 9, 0, 0, 0]
+    # [0, 8, 0, 3, 0, 0, 0, 2, 0]
+    # [7, 1, 0, 0, 8, 0, 0, 3, 5]]
+
+
+
+    insertResult(driver, startingGrid, soup)
 
 if __name__ == '__main__':
     main()
